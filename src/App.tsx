de8ec6dcbe502c2230251567763fb149bce2da0b
Kevin Garay <kevingcfcb88@gmail.com>
2021-03-12 00:08:04 -0600

    Basic Components Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import {PlaceDetails} from './components/PlaceDetails';
+import {SearchBar} from './components/Search';
+import {Grid}from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid centered columns={2} >
+      <SearchBar />
+      <Grid.Row textAlign={'left'}>
+        <PlaceDetails />
+      </Grid.Row>
+    </Grid>
   );
 }
 
---
+++ src/components/PlaceDetails.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Card} from 'semantic-ui-react';
+
+export const PlaceDetails = () => {
+
+    return (
+        <Card>
+            <Card.Content textAlign={"left"}>
+                <Card.Header>Matthew Harris</Card.Header>
+                <Card.Meta>Co-Worker</Card.Meta>
+                <Card.Description>
+                Matthew is a pianist living in Nashville.
+                </Card.Description>
+            </Card.Content>
+        </Card>
+    );
+};
